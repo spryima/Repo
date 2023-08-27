e101dd89f6bf7fbc8aeb088861bf267ebdf8567b
Sergii Pryima <spryima@gmail.com>
2023-08-27 20:12:30 +0300

    first commit

---
+++ goit_homework/clean_folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='clean_folder',
+        version='1.0.1',
+        description='Script to sort your files and clean one folder',
+        url='http://github.com/dummy_user/useful',
+        author='Sergii Pryima',
+        packages=['clean_folder'],
+        entry_points={'console_scripts': ['clean_folder=clean_folder.clean:main']}
+    )
+    
