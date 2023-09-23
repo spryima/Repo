2c0694e5ddd06885fa6e1dcee538c0fec89d3309
Sergii Pryima <spryima@gmail.com>
2023-09-23 08:45:57 +0300

    ready

--- goit_homework/address_book_full/ab_classes.py
+++ goit_homework/address_book_full/ab_classes.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from collections import UserDict
 import re
+import json
 
 
 class InvalidPhoneFormat(Exception):
@@ -49,37 +50,34 @@ class Phone(Field):
                 self._value = value
             else:
                 raise InvalidPhoneFormat(f"Invalid Phone format. Use 10 digits, please")
-
+    def __repr__(self) -> str:
+        return super().__repr__()
 
 class Birthday(Field):
     @property
     def value(self):
-        return self._value.strftime("%d-%m-%Y")
+        return self._value.strftime("%d.%m.%Y")
 
     @value.setter
     def value(self, bday):  
         if re.match(r"[0-3][0-9][.|\\|/|-](([0][1-9])|([1][0-2]))[.|\\|/|-]\d{4}",bday):
             self._value = datetime.strptime(f"{bday[0:2]}-{bday[3:5]}-{bday[-4:]}", "%d-%m-%Y")
         else:
-            raise InvalidBirthdayFormat(f"Invalid Birthday format use -> dd.mm.yyyy")
+            raise InvalidBirthdayFormat(f"Invalid Birthday format. Use -> dd.mm.yyyy")
 
 
 class Contact():
-    def __init__(self, name_obj: Name) -> None:
+    def __init__(self, name_obj: Name, phone_obj: Phone = "", birthday_obj: Birthday = "") -> None:
         self.name = name_obj
-        self.phone = []
-        self.birthday = ""
+        self.phone = [phone_obj] if phone_obj else []
+        self.birthday = birthday_obj if birthday_obj else ""
         
-        
-        
-
     def add_phone(self, phone_obj: Phone):
         if phone_obj.value not in [phone_obj.value for phone_obj in self.phone]:
             self.phone.append(phone_obj)
 
     def add_birthday(self, birthday_obj: Birthday):
         self.birthday = birthday_obj
-
 
     def change_phone(self, old_phone: str, new_phone: str):
         if old_phone in [phone_obj.value for phone_obj in self.phone]:
@@ -91,14 +89,12 @@ class Contact():
     def change_birthday(self, birthday: str):
         self.birthday.value = birthday
         
-
     def __repr__(self) -> str:
         return "Name: {:<10} Phone: {:<15} Birthday: {}".format(
             self.name.value,
             ", ".join(str(ph) for ph in self.phone),
             self.birthday if self.birthday == "" else self.birthday.value,
         )
-
 
 
 class AddressBook(UserDict):
@@ -112,5 +108,6 @@ class AddressBook(UserDict):
 
     def delete(self, name: str):
             del self.data[name]
-
-
+    
+    def delete_all(self):
+        self.data.clear()
--- goit_homework/address_book_full/main.py
+++ goit_homework/address_book_full/main.py
@@ -6,6 +6,12 @@ from ab_classes import (
     AddressBook, 
     )
 import re
+import json
+from pathlib import Path
+
+
+def green_print(text):
+    print(f'\033[92m{text}\033[0m', end="")
 
 
 def input_error(func):
@@ -37,6 +43,7 @@ def add_command(name, *args):
             rec.add_birthday(birthday_obj)
     return f"Add success"
 
+
 def parser(text):
     for cmd, kwds in command_list.items():
         for kwd in kwds:
@@ -48,7 +55,23 @@ def parser(text):
 def unknown_command():
     return f"Unknown command"
 
+
 def exit_command(*_):
+    
+    contacts = []
+
+    for rec in address_book.values():
+        contacts.append(
+            {
+            "name": rec.name.value,
+            "phone": ", ".join(str(ph) for ph in rec.phone),
+            "birthday": rec.birthday if rec.birthday == "" else rec.birthday.value,
+            }
+        )       
+
+    current_dir = Path(__file__).resolve().parent
+    with open(current_dir / "__contacts.json", "w") as fh:
+        json.dump(contacts, fh)
     return f"Good bye!"
     
 
@@ -77,6 +100,11 @@ def show_all_command(*_):
     for rec in address_book.values():
         print(rec)
 
+def delete_all_command(*_):
+    final_check = input(f"\033[91mDo you realy want to Delete Address Book?\033[0m  Y/n >> ")
+    if final_check.lower() == "y":
+        address_book.delete_all()
+
 
 @input_error
 def delete_command(name):
@@ -91,14 +119,9 @@ def help_command(*_):
     return help_message
 
 
-
-
-
-
 help_message = """
                         List of all commands:
 help   - show this list
-
 
 add name [phone] [birthday]         - add New contact (phone and birthday are optional)
 add Bill 0123456789 01.01.1987      - add New contact w phone and birthday
@@ -106,11 +129,9 @@ add Bill                            - add New contact
 add Bill 0123456789                 - add New contact or add New Phone to contact
 add Bill 01.01.1987                 - add New contact or add Birthday to contact
 
-
 find name|number                    - find matches in user names or phone numbers
 find ike                            - find all contacts matching "ike"
 find 0934                           - find all contacts matching phone with "0934"
-
 
 delete Bill
 delete name                         - delete existing contact
@@ -123,16 +144,16 @@ change Bill 1234567890 1111111111   - change Bill's phone number
 change Bill 12.11.1987 01.11.1991   - change Bill's birthday
 change Bill 1231231230              - delete Bill's 1231231230 number
 
-remove phone - 
-add phone - add phone to existing contact
-birthday - show days to birthday 
+delete all                          - delete Address Book
 """
+
 address_book = AddressBook()
 command_list = {
     add_command: ("add", "+"),
     find_command: ("find",),
     change_command: ("change",),
     show_all_command: ("show all", "show"),
+    delete_all_command: ("delete all",),
     delete_command: ("delete", "del"),
     help_command: ("help",),
     exit_command: ("exit", "quit", "good buy"),
@@ -140,6 +161,24 @@ command_list = {
 
 def main():
 
+    # json Address Book import 
+    current_dir = Path(__file__).resolve().parent
+    path_to_json = Path(current_dir / "__contacts.json")
+    if path_to_json.is_file():
+        with open(path_to_json, "r") as fh:
+            records = json.load(fh)
+            for json_rec in records:
+                contact_obj = Contact(
+                    Name(json_rec["name"]),
+                    Phone(json_rec["phone"]) if json_rec["phone"] else "",
+                    Birthday(json_rec["birthday"]) if json_rec["birthday"] else "",
+                    )
+                address_book.add_contact(contact_obj)
+
+    green_print('                 "ADDRESS BOOK"')
+    print()
+    green_print("help  ")
+    print("- to get more information  about commands")
     while True:
         message = input("   >>> ").strip()
 
