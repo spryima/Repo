fe18e6a780be68163e2a99c5ca6e9567fbf42aec
Sergii Pryima <spryima@gmail.com>
2023-10-06 16:23:02 +0300

    minor updates

--- goit_homework/postreSQL_university/SQL_queries/query_10.sql
+++ goit_homework/postreSQL_university/SQL_queries/query_10.sql
@@ -1,8 +1,11 @@
 select distinct subjects.subj_name
 FROM students
-INNER JOIN scoreboard ON students.id = scoreboard.student_id
-INNER JOIN subjects ON scoreboard.subj_name = subjects.subj_name
-INNER JOIN teachers ON subjects.id = teachers.subj_name_id
+INNER JOIN scoreboard 
+ON students.id = scoreboard.student_id
+INNER JOIN subjects 
+ON scoreboard.subj_name = subjects.subj_name
+INNER JOIN teachers 
+ON subjects.id = teachers.subj_name_id
 WHERE students.student_name = 'Хома Остапчук' 
 AND teachers.teacher_name = 'Теодор Савенко'
 ORDER BY subjects.subj_name;
